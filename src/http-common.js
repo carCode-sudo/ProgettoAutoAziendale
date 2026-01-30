f9db8eb03a4db41f1d289c1e4cbf99cd856fb2bb
carCode-sudo <carminecampanile.dev@gmail.com>
2026-01-30 17:30:54 +0100

    update keyclock comunication witch backend token anable 3

--- src/http-common.js
+++ src/http-common.js
@@ -1,9 +1,35 @@
 import axios from "axios";
+import  keycloak  from "./auth/Keycloak";
 
-export default axios.create({
+/* export default axios.create({
     baseURL: '',
     headers: {
         'Content-Type': 'application/json'
     }
 });
+*/
+const httpClient = axios.create({
+    baseURL: '',
+});
+
+httpClient.interceptors.request.use(
+    async (config) => {
+        if (keycloak.authenticated) {
+            try {
+                // Aggiorna il token se scade entro 30 secondi
+                await keycloak.updateToken(30);
+                config.headers.Authorization = `Bearer ${keycloak.token}`;
+            } catch (error) {
+                console.error("Token refresh failed", error);
+                keycloak.login(); // Se il refresh fallisce, rimanda al login
+            }
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default httpClient;
 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import reportWebVitals from './reportWebVitals';
 import {ReactKeycloakProvider} from "@react-keycloak/web";
 import keycloak from "./auth/Keycloak";
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ReactKeycloakProvider authClient={keycloak}>
